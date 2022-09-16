fixed backpropagation, changed GNN structure.

cbf_gnn.py
@@ -4,57 +4,107 @@ from torch_scatter import scatter, scatter_add, scatter_softmax
 from torch.nn import LSTM
 from utils import *
 from config import *
+from torch import Tensor
+from torch_geometric.data import Data
 
 
-class GNN(nn.Module):
+# class GNN(nn.Module):
+#
+#     def __init__(self, feat_dim: int, phi_dim: int, aggr_dim: int, num_agents: int, state_dim: int):
+#         super().__init__()
+#         self.feat_transformer = gnn.Sequential('x, states', [
+#             (GNNLayer(feat_dim, phi_dim, aggr_dim, num_agents, state_dim), 'x, states -> x'),
+#             nn.ReLU(),
+#             (GNNLayer(feat_dim, phi_dim, aggr_dim, num_agents, state_dim), 'x, states -> x'),
+#             ])
+#         self.feat_2_CBF = nn.Sequential(
+#             nn.ReLU(),
+#             nn.Linear(feat_dim, 1)
+#             )
+#         self.x_init = torch.ones((num_agents, feat_dim))
+#
+#     def forward(self, states):
+#         x = self.feat_transformer(self.x_init, states)
+#         h = self.feat_2_CBF(x)
+#         return h
 
-    def __init__(self, feat_dim: int, phi_dim: int, aggr_dim: int, num_agents: int, state_dim: int):
+
+class CBFGNN(nn.Module):
+
+    def __init__(self, state_dim: int, phi_dim: int, num_agents: int):
         super().__init__()
-        self.feat_transformer = gnn.Sequential('x, states', [
-            (GNNLayer(feat_dim, phi_dim, aggr_dim, num_agents, state_dim), 'x, states -> x'),
-            nn.ReLU(),
-            (GNNLayer(feat_dim, phi_dim, aggr_dim, num_agents, state_dim), 'x, states -> x'),
-            ])
-        self.feat_2_CBF = nn.Sequential(
-            nn.ReLU(),
-            nn.Linear(feat_dim, 1)
-            )
-        self.x_init = torch.ones((num_agents, feat_dim))
-
-
-    def forward(self, states):
-        x = self.feat_transformer(self.x_init, states)
-        h = self.feat_2_CBF(x)
-        return h
-
-
-class GNNLayer(gnn.MessagePassing):
-
-    def __init__(self, feat_dim: int, phi_dim: int, aggr_dim: int, num_agents: int, state_dim: int):
-        super(GNNLayer, self).__init__(aggr='mean')
         self.num_agents = num_agents
-        self.phi = MLP(in_channels=feat_dim + state_dim, out_channels=phi_dim, hidden_layers=(20, 20))
-        self.gamma = LSTM(input_size=aggr_dim + feat_dim, hidden_size=feat_dim, num_layers=2)
-        
+        self.feat_transformer = gnn.Sequential('x, edge_index', [
+            (CBFGNNLayer(input_dim=state_dim, output_dim=64, phi_dim=phi_dim), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (CBFGNNLayer(input_dim=64, output_dim=64, phi_dim=phi_dim), 'x, edge_index -> x'),
+        ])
+        self.feat_2_CBF = MLP(in_channels=64, out_channels=num_agents, hidden_layers=(64, 64))
 
-    def forward(self, x, states):
+    def forward(self, data: Data) -> Tensor:
+        """
+        Get the CBF value for the input states.
 
+        Parameters
+        ----------
+        data: Data
+            batched data using Batch.from_data_list().
+
+        Returns
+        -------
+        h: (bs, n, n)
+            CBF values between all agents.
+        """
         # define graph connectivity based on communication radius
-        edge_index = build_comm_links(states, self.num_agents)
+        # edge_index = build_comm_links(states, self.num_agents)
+        x = self.feat_transformer(data.x, data.edge_index)
+        h = self.feat_2_CBF(x)
+        return h.reshape((-1, self.num_agents, self.num_agents))
 
-        # run message passing
-        return self.propagate(edge_index, x=x, states=states)
 
+class CBFGNNLayer(gnn.MessagePassing):
 
-    def message(self, x_j, states_i, states_j):
-        info_ij = torch.cat([x_j, states_j - states_i], dim=1)
+    def __init__(self, input_dim: int, output_dim: int, phi_dim: int):
+        super(CBFGNNLayer, self).__init__(aggr='max')
+        self.phi = MLP(in_channels=3 * input_dim, out_channels=phi_dim, hidden_layers=(64, 64))
+        self.gamma = MLP(in_channels=phi_dim + input_dim, out_channels=output_dim, hidden_layers=(64, 64))
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        return self.propagate(edge_index, x=x)
+
+    def message(self, x_j: Tensor, x_i: Tensor = None) -> Tensor:
+        info_ij = torch.cat([x_i, x_j, x_j - x_i], dim=1)
         return self.phi(info_ij)
 
-
-    # def aggregate(self, phi_out, edge_index):
-    #     return phi_out
-
-
-    def update(self, aggr_out, x):
+    def update(self, aggr_out: Tensor, x: Tensor = None) -> Tensor:
         gamma_input = torch.cat([aggr_out, x], dim=1)
         return self.gamma(gamma_input)
+
+
+# class GNNLayer(gnn.MessagePassing):
+#
+#     def __init__(self, feat_dim: int, phi_dim: int, aggr_dim: int, num_agents: int, state_dim: int):
+#         super(GNNLayer, self).__init__(aggr='mean')
+#         self.num_agents = num_agents
+#         self.phi = MLP(in_channels=feat_dim + state_dim, out_channels=phi_dim, hidden_layers=(128, 128))
+#         self.gamma = LSTM(input_size=aggr_dim + feat_dim, hidden_size=feat_dim, num_layers=2)
+#
+#     def forward(self, x, states):
+#
+#         # define graph connectivity based on communication radius
+#         edge_index = build_comm_links(states, self.num_agents)
+#
+#         # run message passing
+#         return self.propagate(edge_index, x=x, states=states)
+#
+#
+#     def message(self, x_j, states_i, states_j):
+#         info_ij = torch.cat([x_j, states_j - states_i], dim=1)
+#         return self.phi(info_ij)
+#
+#     # def aggregate(self, phi_out, edge_index):
+#     #     return phi_out
+#
+#     def update(self, aggr_out, x):
+#         gamma_input = torch.cat([aggr_out, x], dim=1)
+#         return self.gamma(gamma_input)

train.py
@@ -7,6 +7,8 @@ import os
 import datetime
 
 from tqdm import tqdm
+from torch_geometric.data import Data
+from torch_geometric.data import Batch
 
 import controller_gnn
 import cbf_gnn
@@ -56,8 +58,9 @@ def main():
     num_agents = args.num_agents
     feat_dim = 32
     cbf_controller = controller_gnn.Controller(in_dim=4).to(device)
-    cbf_certificate = cbf_gnn.GNN(feat_dim=feat_dim, phi_dim=feat_dim, aggr_dim=feat_dim,
-                                        num_agents=num_agents, state_dim=4).to(device)
+    # cbf_certificate = cbf_gnn.GNN(feat_dim=feat_dim, phi_dim=feat_dim, aggr_dim=feat_dim,
+    #                               num_agents=num_agents, state_dim=4).to(device)
+    cbf_certificate = cbf_gnn.CBFGNN(state_dim=4, phi_dim=feat_dim, num_agents=num_agents).to(device)
 
     # create optimizers
     optim_controller = optim.Adam(cbf_controller.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
@@ -80,14 +83,15 @@ def main():
         states_curr, goals_curr = generate_data(args.num_agents)
         states_curr = torch.from_numpy(states_curr).to(device)
         goals_curr = torch.from_numpy(goals_curr).to(device)
-        # states_trajectory = []
-        # goals_trajectory = []
-        # actions_trajectory = []
+        states_trajectory = []
+        goals_trajectory = []
+        actions_trajectory = []
+        data_traj = []
 
         # run system for INNER_LOOPS steps to generate consistent trajectory
         for _ in range(INNER_LOOPS):
-            # states_trajectory.append(states_curr)
-            # goals_trajectory.append(goals_curr)
+            states_trajectory.append(states_curr)
+            goals_trajectory.append(goals_curr)
 
             # compute the control input using the trained controller
             actions_curr = cbf_controller(states_curr, goals_curr)
@@ -95,49 +99,55 @@ def main():
                 noise = torch.randn(actions_curr.shape) * NOISE_SCALE
                 actions_curr = actions_curr + noise
 
+            data_traj.append(Data(x=states_curr, edge_index=build_comm_links(states_curr, num_agents)))
+
             # simulate the system for one step
             states_curr = states_curr + dynamics(states_curr, actions_curr) * TIME_STEP
-            # actions_trajectory.append(actions_curr)
+            actions_trajectory.append(actions_curr)
 
-            states_trajectory = states_curr
-            goals_trajectory = goals_curr
-            actions_trajectory = actions_curr
-            # states_trajectory = torch.cat((states_curr,), dim=0)
-            # goals_trajectory = torch.cat((goals_curr,), dim=0)
-            # actions_trajectory = torch.cat((actions_curr,), dim=0)
+            # states_trajectory = states_curr
+            # goals_trajectory = goals_curr
+            # actions_trajectory = actions_curr
 
-            # compute loss for batch of trajectory states
-            h_trajectory = cbf_certificate(states_trajectory)
-            (loss_dang, loss_safe, acc_dang, acc_safe) = loss_barrier(h_trajectory, states_trajectory)
-            (loss_dang_deriv, loss_safe_deriv, acc_dang_deriv, acc_safe_deriv
-            ) = loss_derivatives(states_trajectory, actions_trajectory, h_trajectory, cbf_certificate)
-            loss_action_iter = loss_actions(states_trajectory, goals_trajectory, actions_trajectory)
-            loss_list_iter = [2 * loss_dang, loss_safe, 2 * loss_dang_deriv, loss_safe_deriv, 0.01 * loss_action_iter]
-            acc_list_iter = [acc_dang, acc_safe, acc_dang_deriv, acc_safe_deriv]
-            loss_lists_np.append(loss_list_iter)
-            acc_lists_np.append(acc_list_iter)
-            # total_loss_iter = 10 * torch.add(loss_list_iter + weight_loss)
-            # total_loss_iter = 10 * torch.add(loss_list_iter)
-            # total_loss_iter = torch.Tensor(10 * loss_list_iter)
-            print(loss_list_iter)
-            total_loss_iter = 10 * torch.stack(loss_list_iter).sum()
-
-            # apply optimization step
-            optim_controller.zero_grad()
-            optim_cbf.zero_grad()
-            total_loss_iter.backward(retain_graph=True)
-            optim_controller.step()
-            optim_cbf.step()
-
-            # compute the safety rate
-            # safety_rate = 1 - np.mean(ttc_dangerous_mask_np(states_curr), axis=1)
-            # safety_rate = np.mean(safety_rate == 1)
-            # safety_rates.append(safety_rate)
-            
             if torch.mean(
                 torch.norm(states_curr[:, :2] - goals_curr, dim=1)
             ) < DIST_MIN_CHECK:
                 break
+
+        states_trajectory = torch.cat(states_trajectory, dim=0)
+        goals_trajectory = torch.cat(goals_trajectory, dim=0)
+        actions_trajectory = torch.cat(actions_trajectory, dim=0)
+
+        # compute loss for batch of trajectory states
+        data_traj = Batch.from_data_list(data_traj)
+        h_trajectory = cbf_certificate(data_traj)
+        loss_dang, loss_safe, acc_dang, acc_safe = loss_barrier(h_trajectory, states_trajectory)
+        loss_dang_deriv, loss_safe_deriv, acc_dang_deriv, acc_safe_deriv = \
+            loss_derivatives(states_trajectory, actions_trajectory, h_trajectory, cbf_certificate)
+        loss_action_iter = loss_actions(states_trajectory, goals_trajectory, actions_trajectory)
+        loss_list_iter = [2 * loss_dang, loss_safe, 2 * loss_dang_deriv, loss_safe_deriv, 0.01 * loss_action_iter]
+        acc_list_iter = [acc_dang, acc_safe, acc_dang_deriv, acc_safe_deriv]
+        loss_lists_np.append(loss_list_iter)
+        acc_lists_np.append(acc_list_iter)
+        # total_loss_iter = 10 * torch.add(loss_list_iter + weight_loss)
+        # total_loss_iter = 10 * torch.add(loss_list_iter)
+        # total_loss_iter = torch.Tensor(10 * loss_list_iter)
+        # print(loss_list_iter)
+        total_loss_iter = 10 * torch.stack(loss_list_iter).sum()
+
+        # apply optimization step
+        optim_controller.zero_grad()
+        optim_cbf.zero_grad()
+        total_loss_iter.backward()
+        optim_controller.step()
+        optim_cbf.step()
+
+        # compute the safety rate
+        # safety_rate = 1 - np.mean(ttc_dangerous_mask_np(states_curr), axis=1)
+        # safety_rate = np.mean(safety_rate == 1)
+        # safety_rates.append(safety_rate)
+            
+
 
         # save trained weights
         if i_step % SAVE_STEPS == 0: