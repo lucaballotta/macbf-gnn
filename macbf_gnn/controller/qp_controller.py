7316ae53a13d439c3471429fc06f8dfb455b74cf
luca.ballotta95@gmail.com <luca.ballotta95@gmail.com>
2023-10-12 18:05:02 +0200

    qp controller

--- macbf_gnn/controller/qp_controller.py
+++ macbf_gnn/controller/qp_controller.py
@@ -1,40 +1,41 @@
-import torch.nn as nn
+import torch
+import cvxpy as cp
 
-from torch_geometric.nn import Sequential
-from torch_geometric.nn.conv.transformer_conv import TransformerConv
-from torch import Tensor, cat
 from torch_geometric.data import Data
-
-from macbf_gnn.nn.gnn import ControllerGNNLayer
-from macbf_gnn.nn.mlp import MLP
+from torch import Tensor
 
 from .base import MultiAgentController
+from macbf_gnn.env import MultiAgentEnv
 
 
 class ControllerQP(MultiAgentController):
 
-    def __init__(self, num_agents: int, node_dim: int, edge_dim: int, action_dim: int):
+    def __init__(self, num_agents: int, node_dim: int, edge_dim: int, action_dim: int, env: MultiAgentEnv):
         super(ControllerQP, self).__init__(
             num_agents=num_agents,
             node_dim=node_dim,
             edge_dim=edge_dim,
             action_dim=action_dim
         )
-        self.cbf = 2
+        self._env = env
         
     def forward(self, data: Data) -> Tensor:
-        h = self.cbf(data)
-        actions = []
-        for i_node in range(self.num_agents):
-            action = cp.Variable(self.action_dim)
-            obj = cp.Minimize(cp.sum_squares(action))
-            graph_next = self._env.forward_agent(data, action, i_node)
-            h_next = self.cbf(graph_next)
-            h_dot = (h_next - h) / self._env.dt
-            max_val_h_dot = torch.mean(torch.relu(-h_dot - self.params['alpha'] * h))
-            constraints = [max_val_h_dot <= 0]
-            prob = cp.Problem(obj, constraints)
-            result = prob.solve()
-            actions.append(torch.tensor(action.value).type_as(data.states))
-            
-        return torch.cat(actions)
+        action = cp.Variable((self.num_agents, self.action_dim))
+        obj = cp.Minimize(cp.sum_squares(action))
+        data_next = self._env.forward_graph(data, action)
+        constraints = []
+        for agent in range(self.num_agents):
+            for other_agent in range(self.num_agents):
+                if agent == other_agent:
+                    continue
+                
+                cbf_agent_pair = (data.pos[agent] - data.pos[other_agent])**2 - (2 * self._env._params['car_radius'])**2
+                cbf_next_agent_pair = (data_next.pos[agent] - data_next.pos[other_agent])**2 - (2 * self._env._params['car_radius'])**2
+                cbf_agent_pair_dot = (cbf_next_agent_pair - cbf_agent_pair) / self._env.dt
+                agent_pair_constraint = [cbf_agent_pair_dot + self._env._params['alpha'] * cbf_agent_pair >= 0]
+                constraints.append(agent_pair_constraint)
+                
+        prob = cp.Problem(obj, constraints)
+        _ = prob.solve()
+        
+        return action
